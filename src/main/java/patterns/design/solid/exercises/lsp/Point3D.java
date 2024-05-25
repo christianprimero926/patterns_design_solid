24e5d095f961002bad198a62e60adfb58996fcf3
Christian David Ospina Primero <christianprimero26@hotmail.com>
2024-05-25 00:43:55 -0500

    Exercise and My solution

---
+++ src/main/java/patterns/design/solid/exercises/lsp/Point3D.java
@@ -0,0 +1,42 @@
+package patterns.design.solid.exercises.lsp;
+
+public class Point3D extends Point2D {
+    private final int z;
+
+    public Point3D(int x, int y, int z) {
+        super(x, y);
+        this.z = z;
+    }
+
+
+    public double distance(Point3D other) {
+        int distX = x - other.x;
+        int distY = y - other.y;
+        int distZ = z - other.z;
+        return Math.sqrt( distX * distX + distY * distY + distZ * distZ);
+    }
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + x;
+        result = prime * result + y;
+        result = prime * result + z;
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Point3D other))
+            return false;
+        return x == other.x && y == other.y && z == other.z;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D [x=" + x + ", y=" + y + ", z = " + z + "]";
+    }
+}
---
+++ src/test/java/patterns/design/solid/exercises/lsp/PointTest.java
@@ -0,0 +1,29 @@
+package patterns.design.solid.exercises.lsp;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PointTest {
+    private final Point2D reference = new Point2D(3, 5);
+    private final List<Point2D> test_point = Arrays.asList(
+            new Point2D(2, 3),
+            new Point2D(3, 5),
+            new Point2D(4, 3),
+            new Point3D(3, 5, 1)
+    );
+
+
+    @Test
+    void test_equal_if_only_if_distance_0() {
+
+        for (Point2D point : test_point) {
+            assertEquals(point.equals(reference), point.distance(reference) == 0, "test para " + reference + " y " + point);
+        }
+    }
+
+}
